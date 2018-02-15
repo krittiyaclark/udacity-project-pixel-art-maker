d5eec02aac56e767f2cd48ab9f773df06431400b
Krittiya Clark <krittiyaclark@gmail.com>
2018-02-14 23:10:35 -0500

    Create grid when button is clicked

--- designs.js
+++ designs.js
@@ -2,30 +2,29 @@
 // TODO: Add event listeners
 // TODO: Set the size of the cross stitch canvas
 
-const grid = $('#pixelCanvas');
-const colorPicker = $('#colorPicker');
-const inputHeight = $('#inputHeight').val();
-const inputWidth = $('#inputWidth').val();
-
-// Select color input
-// Select size input
-
-// When size is submitted by the user, call makeGrid()
-
-function makeGrid(w, h) {
-  for (var row = 0; row <= w; row++) {
-    const row = $('<tr class="row"></tr>');
-    for (var col = 0; col <= h; col++) {
-      row.append($('<td class="column"></td>'))
-    }
-    grid.append(row);
-  }
-}
+$(document).ready(function() {
 
   $('#submit').click(function(e) {
-    e.preventDefault();
-    makeGrid(inputHeight, inputWidth);
+     event.preventDefault();
+     makeGrid();
   });
 
+  function makeGrid() {
 
-$(makeGrid);
+    // Select value
+    let grid = $('#pixelCanvas');
+    let colorPicker = $('#colorPicker');
+    let inputHeight = $('#inputHeight').val();
+    let inputWidth = $('#inputWidth').val();
+
+    // Create grid 
+    for (let row = 1; row <= inputHeight; row++) {
+      let makeRow = $('<tr></tr>');
+      for (let col = 1; col <= inputWidth; col++) {
+        makeRow.append($('<td></td>'))
+      }
+      grid.append(makeRow);
+    }
+  }
+
+});
